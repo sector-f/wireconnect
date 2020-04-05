22b3bde98d0223ffab704c54021c72e95e3bdcf0
Adam <sector-f@users.noreply.github.com>
2020-04-05 01:35:00 -0400

    Cleaned up types now that HTTP is being used

--- cmd/wireconnect-server/main.go
+++ cmd/wireconnect-server/main.go
@@ -2,20 +2,9 @@ package main
 
 import (
 	"log"
-	"net/http"
 
-	"github.com/sector-f/wireconnect"
 	"github.com/sector-f/wireconnect/cmd/wireconnect-server/server"
 )
-
-var auth wireconnect.AuthProvider = mockAuth{}
-
-type Route struct {
-	Pattern    string
-	Method     string
-	Handler    http.HandlerFunc
-	Permission string
-}
 
 func main() {
 	config := server.NewConfig()
--- wireconnect.go
+++ wireconnect.go
@@ -1,25 +1,11 @@
 package wireconnect
 
-type AuthProvider interface {
-	Auth(req Request) (Reply, error)
-}
-
 type Request struct {
-	Username  string `json:"username"`
-	Password  string `json:"password"`
 	PublicKey string `json:"public_key"`
 }
 
 type Reply struct {
-	Error         string `json:"error"`
 	PublicKey     string `json:"public_key"`
 	ClientAddress string `json:"client_address"`
 	ServerAddress string `json:"server_address"`
-}
-
-type User struct {
-	Username     string
-	Password     string
-	Address      string
-	PresharedKey string
 }
